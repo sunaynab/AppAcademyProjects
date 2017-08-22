0554e9040d36594777eb81bd040e1543ebb54b39
sunaynab <sunaynabhikha@gmail.com>
2017-08-22 09:28:04 -0700

    Update Artwork and ArtworkShares Controllers

--- W4D1/first_routes/db/seeds.rb
+++ W4D1/first_routes/db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+u1 = User.create(username: Faker::Internet.user_name)
+u2 = User.create(username: Faker::Internet.user_name)
+u3 = User.create(username: Faker::Internet.user_name)
+u4 = User.create(username: Faker::Internet.user_name)
+
+a1 = Artwork.create(image_url: Faker::Internet.url, artist_id: 2, title: Faker::HarryPotter.location)
+a2 = Artwork.create(image_url: Faker::Internet.url, artist_id: 3, title: Faker::HarryPotter.location)
+a3 = Artwork.create(image_url: Faker::Internet.url, artist_id: 2, title: Faker::HarryPotter.location)
+a4 = Artwork.create(image_url: Faker::Internet.url, artist_id: 2, title: Faker::HarryPotter.location)
+
+as1 = ArtworkShare.create(artwork_id: 1, viewer_id: 1)
+as2 = ArtworkShare.create(artwork_id: 1, viewer_id: 3)
+as3 = ArtworkShare.create(artwork_id: 1, viewer_id: 4)
+as4 = ArtworkShare.create(artwork_id: 2, viewer_id: 2)
+as5 = ArtworkShare.create(artwork_id: 3, viewer_id: 4)
