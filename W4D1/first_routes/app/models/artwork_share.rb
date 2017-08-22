82ffe71dd297e52199d1fd3072a8f6619648a54d
sunaynab <sunaynabhikha@gmail.com>
2017-08-21 18:40:22 -0700

    Add ArtShare models and migrations

---
+++ W4D1/first_routes/app/models/artwork_share.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+#
+# Table name: artwork_shares
+#
+#  id         :integer          not null, primary key
+#  artwork_id :integer          not null
+#  viewer_id  :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class ArtworkShare < ApplicationRecord
+  validates :artwork_id, :viewer_id, presence: true
+  validates :artwork_id, uniqueness: {scope: :viewer_id, message: "The same artwork should not be shown to the same viewer twice." }
+
+  belongs_to :artwork,
+  primary_key: :id,
+  foreign_key: :artwork_id,
+  class_name: :Artwork
+
+  belongs_to :viewer,
+  primary_key: :id,
+  foreign_key: :viewer_id,
+  class_name: :User
+end
--- W4D1/first_routes/db/migrate/20170821222054_create_users.rb
+++ W4D1/first_routes/db/migrate/20170821222054_create_users.rb
@@ -1,10 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string :name, null: false
-      t.string :email, null: false
+      t.string :username, null: false
       t.timestamps
     end
-
+    add_index :users, :username, unique: true
   end
 end
